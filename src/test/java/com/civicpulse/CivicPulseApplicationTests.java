9e9b9d59f508812ea187d14c91e1e43328e2c605
Demo-163 <dd6070270@gmail.com>
2026-08-04 14:53:08 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/civicpulse/CivicPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.civicpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CivicPulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
